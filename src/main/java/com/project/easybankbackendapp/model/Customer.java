2d7acf4ffae47902f290429102c1e3293a4f9ddd
Thejas318 <tejasgowda.ys8@gmail.com>
2024-04-13 00:01:56 -0400

    Added customer entity and customer repository

---
+++ src/main/java/com/project/easybankbackendapp/model/Customer.java
@@ -0,0 +1,42 @@
+package com.project.easybankbackendapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+public class Customer {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String pwd;
+
+    @NotNull
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/project/easybankbackendapp/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.project.easybankbackendapp.repository;
+
+import com.project.easybankbackendapp.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    List<Customer> findByEmail(String email);
+}
